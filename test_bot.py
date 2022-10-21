9ed8d3698b96529ea3c947bc6831f8753916d224
IgnacioVitale <vitale.ignacio.93@gmail.com>
2022-10-21 13:23:50 +0200

    Added on message delete event

--- test_bot.py
+++ test_bot.py
@@ -16,8 +16,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('./hello'):
+        await message.channel.send('Sup fam')
+
+
+@client.event
+async def on_message_delete(message):
+    if message.author == client.user:
+        await message.channel.send('Me estan censurando en vivo')
+    else:
+        await message.channel.send('Vi lo que borraste, picaron')
 
 
 client.run('MTAzMjk2NTY5OTYyMDA1MzA1Mw.GR4Qke.xLPeqa18P8xFBzK5rtCV-VgG9hA8PTZR0nSUZ0')
