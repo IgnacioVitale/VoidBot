1c3d26e8b63bda8da6c145b723f887e087fa3f6c
Salvador Villaschi <salwy2704@gmail.com>
2022-10-22 19:17:29 -0300

    Added the mention on coinflip

--- on_message_helpers/coin_flip.py
+++ on_message_helpers/coin_flip.py
@@ -1,11 +1,14 @@
 import random
 
+from DiscordBot.utils import mention_id
+
 
 async def coin_flip(ctx):
+    user = ctx.message.author.id
     coin_head = "head"
     coin_tails = "tails"
     coin_throw = random.randint(1, 2)
     if coin_throw == 1:
-        await ctx.channel.send(f"Your coin landed on {coin_head}")
+        await ctx.channel.send(f"{mention_id(user)} Your coin landed on {coin_head}")
     else:
-        await ctx.channel.send(f"Your coin landed on {coin_tails}")
+        await ctx.channel.send(f"{mention_id(user)} Your coin landed on {coin_tails}")
